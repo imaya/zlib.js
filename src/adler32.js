076b13eee31d60af5d1fa85a663c25094e0b00e4
Yuta Imaya <yuta.imaya@gree.net>
2012-04-17 14:58:14 +0900

    optimize adler32

--- src/adler32.js
+++ src/adler32.js
@@ -52,17 +52,40 @@ Zlib.Adler32 = function(array) {
  * @return {number} Adler32 ハッシュ値.
  */
 Zlib.Adler32.update = function(adler, array) {
-  var s1 = adler & 0xffff,
-      s2 = (adler >>> 16) & 0xffff;
+  /** @type {number} */
+  var s1 = adler & 0xffff;
+  /** @type {number} */
+  var s2 = (adler >>> 16) & 0xffff;
+  /** @type {number} array length */
+  var len = array.length;
+  /** @type {number} loop length (don't overflow) */
+  var tlen;
+  /** @type {number} array index */
+  var i = 0;
 
-  for (var i = 0, l = array.length; i < l; i++) {
-    s1 = (s1 + array[i]) % 65521;
-    s2 = (s2 + s1) % 65521;
+  while (len > 0) {
+    tlen = len > Zlib.Adler32.OptimizationParameter ?
+      Zlib.Adler32.OptimizationParameter : len;
+    len -= tlen;
+    do {
+      s1 += array[i++];
+      s2 += s1;
+    } while (--tlen);
+
+    s1 %= 65521;
+    s2 %= 65521;
   }
 
   return ((s2 << 16) | s1) >>> 0;
 };
 
+/**
+ * Adler32 最適化パラメータ
+ * 現状では 1024 程度が最適.
+ * @see http://jsperf.com/adler-32-simple-vs-optimized/3
+ * @const {number}
+ */
+Zlib.Adler32.OptimizationParameter = 1024;
 
 // end of scope
 });
