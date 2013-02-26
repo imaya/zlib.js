476b01764084bf2553c97540b1124082eb18d8c8
Yuta Imaya <yuta.imaya@gree.net>
2013-02-26 14:01:22 +0900

    add chrome test

---
+++ .travis/chrome-start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#Variables
+USER_DIR=/tmp/buster
+MAC_CMD="/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary"
+UBUNTU_CMD="google-chrome"
+
+# Test OS
+if [ -f "$MAC_CMD" ]
+then
+	CMD="$MAC_CMD"
+else
+	CMD="$UBUNTU_CMD"
+fi
+
+# Prepare
+rm -rf $USER_DIR
+mkdir -p $USER_DIR"/Default/"
+
+# Execute the command
+exec "$CMD" --user-data-dir="$USER_DIR" --no-default-browser-check --no-first-run --disable-default-apps "$@"
